799f636d23f56ee23ba30157b5d140502291ba14
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-06 10:29:31 +0000

    arrey

---
+++ 6arrey.sh
@@ -0,0 +1,5 @@
+#!/bin/bash/
+#it can called as arrey,insted of single value it can hold multiple values
+PERSONS=("PAVAN" "RAMESH" "SURESH")
+#inside the arrey we always refers the vaules with index and its starts from 0
+echo "first person: ${persons[0]}"
