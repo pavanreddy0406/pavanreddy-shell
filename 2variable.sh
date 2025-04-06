a1c46ce9789f0a8bc77e88fac1f3614f2b6ef533
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-06 09:35:38 +0000

    date commend

--- 2variable.sh
+++ 2variable.sh
@@ -1,4 +1,5 @@
 #!/bin/bash/
+
 #shellscript will run the command $()and it stores the output into varible called DATE
 DATE=$(date)
-echo "this script excuted at timestamp:$DATE
+echo "this script excuted at timestamp: $DATE
