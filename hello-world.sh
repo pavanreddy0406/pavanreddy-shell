95ce54e96a0a7b5c5cdb31c28bf264021c86e085
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-06 09:02:43 +0000

    without varibles

--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,9 @@
 #!/bin/bash/
 echo "iam learning"
-echo"iam also devops groups"
+echo "iam also devops groups"
+echo "pavan:hello,ramesh
+echo "ramesh:hello pavan
+echo "pavan:where are you ramesh
+echo "ramesh:iam fine pavan
+echo "pavan:ok
+echo "ramesh:ok
