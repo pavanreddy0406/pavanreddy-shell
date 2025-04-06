cb03f3607917257ca3a4b12faf1e44cf9c669085
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-06 09:44:17 +0000

    this the get values of varibles of commamd line

---
+++ 3variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash/
+echo "iam learning"
+echo "iam also devops groups"
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1:hello,$PERSON2
+echo "$PERSON2:hello $PERSON1
+echo "$PERSON1:where are you $PERSON2
+echo "$PERSON2:iam fine $PERSON1
+echo "$PERSON1:ok
+echo "$PERSON2:ok
