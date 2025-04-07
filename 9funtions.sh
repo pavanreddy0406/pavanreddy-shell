933e3c6bf0ca2bef43266a791f981966facac20b
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-07 07:12:57 +0000

    short method of fauntions

---
+++ 9funtions.sh
@@ -0,0 +1,25 @@
+#!/bin/bash/
+#our program goal is installing my sql and postfix
+USERID=$(id -u)
+VALIDATE(){
+if [$1 -ne 0 ]
+then
+    echo "$2 .... failure"
+    exit1
+else
+    echo "$2.... is success"
+fi
+}
+if [$USERNAME -ne 0 ]
+exit 1
+then
+    echo "ERROR:run the sceipt in root acess"
+else
+    echo "INFO: suess
+fi
+yum install mysql -y
+VALIDATE $? "installing mysql"
+
+yum install postfix -y
+VALIDATE $? "installing postfix
+
