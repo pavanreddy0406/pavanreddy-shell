c679d8ba7daacf596279006e7c323e1ae7164b57
pavanreddy0406 <pavanreddyyeddala@gmail.com>
2025-04-07 09:21:01 +0000

    special vars

---
+++ 10logfiles.sh
@@ -0,0 +1,26 @@
+#!/bin/bash/
+#our program goal is installing my sql and postfix
+DATE=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/
+USERID=$(id -u)
+VALIDATE(){
+if [$1 -ne 0 ]
+then
+    echo "$2 .... failure"
+    exit 1
+else
+    echo "$2.... is success"
+fi
+}
+if [$USERID -ne 0 ]
+then
+    echo "ERROR:run the sceipt in root acess"
+    exit 1
+else
+    echo "INFO: success"
+fi
+yum install mysql -y
+VALIDATE $? "installing mysql"
+
+yum install postfix -y
+VALIDATE $? "installing postfix"
---
+++ 11special-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash/
+var1=$1
+var2=$2 
+echo "script name: $0
+echo "variable 1: $var1
+echo "all var: $@"
+echo "no of vars: $#
